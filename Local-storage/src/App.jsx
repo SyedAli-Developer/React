c3c976a595483f0efe29986d02d4327e225de663
SyedAli-Developer <syedali.h.b.g.626@gmail.com>
2026-05-14 09:22:08 +0500

    Practice 1 complete

--- Local-storage/src/App.jsx
+++ Local-storage/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const App = () => {
-  
-  
-  
+
+  async function getdata() {
+    const data = await fetch('https://jsonplaceholder.typicode.com/users')
+    const converted = await data.json(data)
+    console.log(converted)
+  }
   return (
     <div>
-      
+      <div>
+        
+      </div>
+      <button onClick={getdata}>Get Information</button>
     </div>
   )
 }
